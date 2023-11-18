a8a6eb115859a6150e23d23679a9ceedb2c36ec5
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-18 20:00:17 +0200

    Added Account data entity

---
+++ smart-expenses-web-app/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using smart_expenses_web_app.Enums;
+
+namespace smart_expenses_web_app.Models;
+
+public class Account
+{
+    [Key]
+    public long Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public AccountType AccountType { get; set; }
+
+    public string? Title { get; set; }
+
+    public decimal Amount { get; set; }
+    
+    public CurrencyCode CurrencyCode { get; set; }
+    
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; } = null!;
+    
+    public ICollection<Transaction> Transactions { get; set; } = null!;
+}
