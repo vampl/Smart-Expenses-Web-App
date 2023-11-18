ef9c1045ae44f7fdd961f2202dbe74d15a643305
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-18 20:01:11 +0200

    Added navigation properties to user identity entity

--- smart-expenses-web-app/Models/User.cs
+++ smart-expenses-web-app/Models/User.cs
@@ -4,5 +4,8 @@ namespace smart_expenses_web_app.Models;
 
 public class User : IdentityUser
 {
+    public ICollection<Account> Accounts { get; set; } = null!;
+
+    public ICollection<Transaction> Transactions { get; set; } = null!;
 }
 
