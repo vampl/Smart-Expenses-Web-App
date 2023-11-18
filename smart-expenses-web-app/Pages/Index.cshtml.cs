2d07a205216b462797567d86793935ea6c2124d6
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-18 19:21:27 +0200

    Cleanup

--- smart-expenses-web-app/Pages/Index.cshtml.cs
+++ smart-expenses-web-app/Pages/Index.cshtml.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace smart_expenses_web_app.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
+    
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
-
+    
     public void OnGet()
     {
     }
