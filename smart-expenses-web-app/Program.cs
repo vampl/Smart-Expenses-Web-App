d66b152b842b5ae3f250be70045120e79c261d7c
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-18 19:21:11 +0200

    Added data context connection and identity, configure identity

--- smart-expenses-web-app/Program.cs
+++ smart-expenses-web-app/Program.cs
@@ -1,6 +1,45 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using smart_expenses_web_app.Data;
+using smart_expenses_web_app.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Get context connection string
+var connectionString = builder.Configuration.GetConnectionString("SmartExpensesDataContextConnection") ??
+                       throw new InvalidOperationException("Connection string 'SmartExpensesDataContextConnection' not found.");
+
+// Add context to application services
+builder.Services.AddDbContext<SmartExpensesDataContext>(options => options.UseSqlite(connectionString));
+
+// Add Identity to application services
+builder.Services
+    .AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<SmartExpensesDataContext>();
+
+// Configure Identity service
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password secure settings
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+    
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+// Add razor pages to application services
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -9,7 +48,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days.
     app.UseHsts();
 }
 
@@ -18,6 +57,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
