a7f45d406e21ffd4db59fd7c512e2a3828488321
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-21 19:43:28 +0200

    Modified data model by data annotation to properties

--- smart-expenses-web-app/Models/Transaction.cs
+++ smart-expenses-web-app/Models/Transaction.cs
@@ -7,22 +7,32 @@ namespace smart_expenses_web_app.Models;
 public class Transaction
 {
     [Key]
+    [Display(Name = "Ідентифікатор")]
     public long Id { get; set; }
 
+    [Display(Name = "Ідентифікатор користувача")]
     public string UserId { get; set; } = null!;
     
+    [Display(Name = "Ідентифікатор рахунку")]
     public long AccountId { get; set; }
 
+    [Display(Name = "Тип транзакцій")]
     public TransactionType TransactionType { get; set; }
     
+    [Display(Name = "Найменування")]
     public string? Title { get; set; }
     
+    [Display(Name = "Сума")]
     public decimal Amount { get; set; }
-
+    
+    [Display(Name = "Валюта")]
     public CurrencyCode CurrencyCode { get; set; }
 
+    [Display(Name = "Дата")]
+    [DataType(DataType.Date)]
     public DateTime DateTime { get; set; }
     
+    [Display(Name = "Опис")]
     public string? Description { get; set; }
     
     [ForeignKey(nameof(UserId))]
