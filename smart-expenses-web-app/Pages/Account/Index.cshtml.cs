2bf5c3fd7e662efb6814e7577345dad7b59467e0
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-21 22:38:05 +0200

    Modified index model to select current user accounts

--- smart-expenses-web-app/Pages/Account/Index.cshtml.cs
+++ smart-expenses-web-app/Pages/Account/Index.cshtml.cs
@@ -1,26 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using smart_expenses_web_app.Data;
+using smart_expenses_web_app.Models;
 
 namespace smart_expenses_web_app.Pages.Account;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     private readonly SmartExpensesDataContext _context;
-
-    public IndexModel(SmartExpensesDataContext context)
+    private readonly UserManager<User> _userManager;
+    
+    public IndexModel(SmartExpensesDataContext context, UserManager<User> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
 
     public IList<Models.Account> Account { get;set; } = default!;
 
     public async Task OnGetAsync()
     {
-        if (_context.Accounts != null)
-        {
-            Account = await _context.Accounts
-                .Include(a => a.User).ToListAsync();
-        }
+        var user = await _userManager.GetUserAsync(User);
+        var userId = await _userManager.GetUserIdAsync(user ?? throw new InvalidOperationException());
+
+        Account = await _context.Accounts
+            .Include(a => a.User)
+            .Where(a => a.UserId == userId)
+            .ToListAsync();
     }
 }
