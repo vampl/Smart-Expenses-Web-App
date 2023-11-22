3b4319b094f1eeaa310f0838ccea1e76a5d0acd8
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-22 15:55:41 +0200

    Added Authorize security, cleanup

--- smart-expenses-web-app/Pages/Account/Delete.cshtml.cs
+++ smart-expenses-web-app/Pages/Account/Delete.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -5,6 +6,7 @@ using smart_expenses_web_app.Data;
 
 namespace smart_expenses_web_app.Pages.Account;
 
+[Authorize]
 public class DeleteModel : PageModel
 {
     private readonly SmartExpensesDataContext _context;
@@ -19,7 +21,7 @@ public class DeleteModel : PageModel
 
     public async Task<IActionResult> OnGetAsync(long? id)
     {
-        if (id == null || _context.Accounts == null)
+        if (id == null)
         {
             return NotFound();
         }
@@ -32,23 +34,29 @@ public class DeleteModel : PageModel
         }
 
         Account = account;
+        
         return Page();
     }
 
     public async Task<IActionResult> OnPostAsync(long? id)
     {
-        if (id == null || _context.Accounts == null)
+        if (id == null)
         {
             return NotFound();
         }
+        
         var account = await _context.Accounts.FindAsync(id);
 
-        if (account != null)
+        if (account == null)
         {
-            Account = account;
-            _context.Accounts.Remove(Account);
-            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
+        
+        Account = account;
+        
+        _context.Accounts.Remove(Account);
+        
+        await _context.SaveChangesAsync();
 
         return RedirectToPage("./Index");
     }
