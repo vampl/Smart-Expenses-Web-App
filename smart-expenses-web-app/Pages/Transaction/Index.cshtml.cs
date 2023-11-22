d46a90e6eb9c167ee6abaaf6a405cc5ce297a4ae
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-22 16:08:45 +0200

    Added Authorize to model, updated user get transaction logic

--- smart-expenses-web-app/Pages/Transaction/Index.cshtml.cs
+++ smart-expenses-web-app/Pages/Transaction/Index.cshtml.cs
@@ -1,27 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using smart_expenses_web_app.Data;
+using smart_expenses_web_app.Models;
 
 namespace smart_expenses_web_app.Pages.Transaction;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     private readonly SmartExpensesDataContext _context;
-
-    public IndexModel(SmartExpensesDataContext context)
+    private readonly UserManager<User> _userManager;
+    
+    public IndexModel(SmartExpensesDataContext context, UserManager<User> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
 
     public IList<Models.Transaction> Transaction { get;set; } = default!;
 
     public async Task OnGetAsync()
     {
-        if (_context.Transactions != null)
-        {
-            Transaction = await _context.Transactions
-                .Include(t => t.Account)
-                .Include(t => t.User).ToListAsync();
-        }
+        var user = await _userManager.GetUserAsync(User);
+        var userId = await _userManager.GetUserIdAsync(user ?? throw new InvalidOperationException());
+        
+        Transaction = await _context.Transactions
+            .Include(t => t.Account)
+            .Include(t => t.User)
+            .Where(t => t.UserId == userId)
+            .ToListAsync();
     }
 }
