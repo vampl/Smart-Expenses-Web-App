a6c1baf01ffdffc8d8f4fbb753ad97b056e328d3
Vitalii Barabash <vitalii.barabash.2002.activity@gmail.com>
2023-11-24 19:48:30 +0200

    Added comments, refactored exist checkup

--- smart-expenses-web-app/Pages/Transaction/Delete.cshtml.cs
+++ smart-expenses-web-app/Pages/Transaction/Delete.cshtml.cs
@@ -11,49 +11,54 @@ public class DeleteModel : PageModel
 
     public DeleteModel(SmartExpensesDataContext context)
     {
+        // Inject required services
         _context = context;
+
+        Transaction = new Models.Transaction();
     }
 
     [BindProperty] 
-    public Models.Transaction Transaction { get; set; } = default!;
+    public Models.Transaction Transaction { get; set; }
 
     public async Task<IActionResult> OnGetAsync(long? id)
     {
+        // Check if Id passed
         if (id == null)
         {
             return NotFound();
         }
 
-        var transaction = await _context.Transactions
-            .Include(t=> t.Account)
-            .FirstOrDefaultAsync(m => m.Id == id);
-
+        // Check if transaction is exist in database
+        var transaction = await _context.Transactions.FirstOrDefaultAsync(m => m.Id == id);
         if (transaction == null)
         {
             return NotFound();
         }
 
+        // Fill transaction form
         Transaction = transaction;
+        
         return Page();
     }
 
     public async Task<IActionResult> OnPostAsync(long? id)
     {
+        // Check if Id passed
         if (id == null)
         {
             return NotFound();
         }
+        
+        // Check if transaction is exist in database
         var transaction = await _context.Transactions.FindAsync(id);
-
         if (transaction == null)
         {
             return RedirectToPage("./Index");
         }
         
+        // Delete transaction from database & save
         Transaction = transaction;
-            
         _context.Transactions.Remove(Transaction);
-            
         await _context.SaveChangesAsync();
 
         return RedirectToPage("./Index");
